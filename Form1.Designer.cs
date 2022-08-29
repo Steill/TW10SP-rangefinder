8d977749424626ea73e563b5a732691f2e9a0b14
oesipenko <oesipenko@ESIPENKO-PC>
2022-08-29 17:17:58 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace rangefinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(95, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Select COM";
+            this.comboBox1.DropDown += new System.EventHandler(this.comboBox1_DropDown);
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(12, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "MEASURE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(113, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(360, 121);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "CLEAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 146);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "RangeFinder";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Text;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace rangefinder
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort port_ = null;
+        private StringBuilder sb = new StringBuilder(0x20);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (port_ == null)
+                return;
+
+            Button btn = (Button)sender;
+            if (port_.IsOpen)
+            {
+                byte[] command = { 0x1, 0x3, 0x0, 0xF, 0x0, 0x2, 0xF4, 0x8 };
+
+                try
+                {
+                    port_.Write(command, 0, 8);
+
+                    btn.Enabled = false;
+                    timer1.Start();
+                }
+                catch
+                {
+                    MessageBox.Show("COM Device disconnected");
+                    port_.Close();
+                    port_ = null;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void comboBox1_DropDown(object sender, EventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            comboBox.Items.Clear();
+
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+                comboBox.Items.Add(port);
+
+            listBox1.Items.Insert(0, " -<- COM Ports Refreshed ->- ");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (port_ != null && port_.IsOpen)
+            {
+                port_.Close();
+                port_ = null;
+            }
+
+            ComboBox comboBox = (ComboBox)sender;
+            port_ = new SerialPort(comboBox.SelectedItem.ToString(), 9600, Parity.None, 8, StopBits.One);
+            port_.DataReceived += new SerialDataReceivedEventHandler(port_dataRecieved);
+
+            try
+            {
+                port_.Open();
+
+                comboBox1.Enabled = false;
+                timer2.Start();
+            }
+            catch
+            {
+                MessageBox.Show("COM Device busy");
+                return;
+            }
+        }
+
+        private void port_dataRecieved(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort serialPort = (SerialPort)sender;
+            
+            sb.Append(serialPort.ReadExisting());
+
+            if (sb.Length < 9)
+                return;
+
+            byte[] data = Encoding.ASCII.GetBytes(sb.ToString());
+
+            if (data[0] == 0x1 && data[1] == 0x3 && data[2] == 0x4)
+            {
+                string hex_view = "";
+                foreach (byte b in data)
+                    hex_view += String.Format("0x{0:X2} ", b);
+
+                int value = 0;
+
+                if (BitConverter.IsLittleEndian)
+                {
+                    Array.Reverse(data); 
+                    value = BitConverter.ToInt32(data, 2);
+                }
+                else 
+                    value = BitConverter.ToInt32(data, 3);
+
+                DateTime time = DateTime.Now;
+                string result = String.Format("{2:D2}:{3:D2}:{4:D2} - {0}мм [ {1}]", value, hex_view, time.Hour, time.Minute, time.Second);
+
+                listBox1.Invoke((MethodInvoker)delegate
+                {
+                    listBox1.Items.Insert(0, result);
+                });
+            }
+            sb.Clear();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Timer timer = (Timer)sender;
+
+            timer.Enabled = false;
+            button1.Enabled = true;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Timer timer = (Timer)sender;
+
+            timer.Enabled = false;
+
+            comboBox1.Enabled = true;
+            button1.Enabled = true;
+
+            button1.Focus();
+        }
+    }
+}
